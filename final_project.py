d2f499112fa56eb884b0cb5e8333ccc131426d96
arjun-shah-st <arjunrs@stanford.edu>
2024-12-02 16:37:37 -0800

    kind of works

--- final_project.py
+++ final_project.py
@@ -17,10 +17,10 @@ class RL_agent:
                  gamma_c, 
                  Q_r,
                  Q_c,
-                 max_elements=40,
+                 max_elements=25,
                  problems = [LinearAdvection(1, 0.8, 0)],
-                 endtimes = [0.5],
-                 u0s = [lambda x: 1-np.tanh(2*(1-4*(x-1/4)))]):
+                 endtimes = [0.01],
+                 u0s = [lambda x: 1-np.tanh(2*(1-4*(x - 1/4)))]):
         """
         Parameters: 
         -----------
@@ -89,10 +89,10 @@ class RL_agent:
         training_coarsen_outputs: np.ndarray 
             The reward for coarsening (nn_c output)
         """
-        training_refine_inputs = np.empty((n_iterations, 8))
+        training_refine_inputs = np.empty((n_iterations, 6))
         training_refine_outputs = np.empty(n_iterations)
-        training_coarsen_inputs = np.empty((n_iterations * 2 - 2, 8))
-        training_coarsen_outputs = np.empty(n_iterations * 2 - 2)
+        training_coarsen_inputs = np.empty((n_iterations -1, 6))
+        training_coarsen_outputs = np.empty(n_iterations - 1)
         x = copy.deepcopy(x0)
         interval = 0
         sol = None 
@@ -110,11 +110,8 @@ class RL_agent:
             training_refine_outputs[i] = outputs
             
             if i != 0: 
-                training_coarsen_inputs[2 * i - 2] = coarsen_inputs_l
-                training_coarsen_inputs[2 * i - 1] = coarsen_inputs_r
-
-                training_coarsen_outputs[2 * i - 2] = -training_refine_outputs[i-1]
-                training_coarsen_outputs[2 * i - 1] = -training_refine_outputs[i-1]
+                training_coarsen_inputs[i - 1] = coarsen_inputs_l
+                training_coarsen_outputs[i - 1] = -training_refine_outputs[i-1]
                 
             # if a flag is raised, reset to initial case and continue generating data
             if flag: 
@@ -126,7 +123,7 @@ class RL_agent:
 
         return training_refine_inputs, training_refine_outputs, training_coarsen_inputs, training_coarsen_outputs
 
-    def train_step(self, x, problem, u0, endtime, old_interval, sol = None):
+    def train_step(self, x, problem, u0, endtime, old_interval, sol = None, epsilon=1):
         """
         A single step of generating training data 
 
@@ -197,7 +194,13 @@ class RL_agent:
         
         avg_gradient = np.mean(gradients)
 
-        interval_choice = np.random.randint(0, len(gradients))
+        if np.random.rand()< epsilon: 
+            interval_choice = np.random.randint(0, len(gradients))
+        else: 
+            inputs = np.array([gradients, np.full(gradients.shape, avg_gradient), delta_x, np.full(gradients.shape, np.mean(delta_x)), np.full(gradients.shape, p), np.full(gradients.shape, c)]).T
+            r_outputs = self.Q_r.predict(inputs)
+            interval_choice = np.argmax(r_outputs)
+
         local_gradient = gradients[interval_choice]
 
         if interval_choice == 0:
@@ -214,7 +217,7 @@ class RL_agent:
 
         average_size = np.mean(delta_x)
 
-        training_inputs = [local_gradient, lg, rg, avg_gradient, size, average_size, p, c]
+        training_inputs = [local_gradient, avg_gradient, size, average_size, p, c]
 
         if calculate_coarsen_values: 
             local_gradient_old_l = gradients[old_interval]
@@ -237,12 +240,12 @@ class RL_agent:
             size_old_l = delta_x[old_interval]
             size_old_r = delta_x[old_interval + 1]
 
-            training_inputs_coarsen_l = [local_gradient_old_l, lg_old_l, rg_old_l, avg_gradient, size_old_l, average_size, p, c]
-            training_inputs_coarsen_r = [local_gradient_old_r, lg_old_r, rg_old_r, avg_gradient, size_old_r, average_size, p, c]
+            training_inputs_coarsen_l = [local_gradient_old_l, avg_gradient, size_old_l, average_size, p, c]
+            training_inputs_coarsen_r = [local_gradient_old_r, avg_gradient, size_old_r, average_size, p, c]
 
         else: 
-            training_inputs_coarsen_l = np.full(8, np.nan)
-            training_inputs_coarsen_r = np.full(8, np.nan)
+            training_inputs_coarsen_l = np.full(6, np.nan)
+            training_inputs_coarsen_r = np.full(6, np.nan)
 
         new_x = np.insert(x, interval_choice+1, 1/2* (x[interval_choice] + x[interval_choice + 1]))
         new_u0 = u0(new_x)
@@ -253,6 +256,7 @@ class RL_agent:
         projected_original_sol = np.insert(original_sol, interval_choice+1, 1/2 * (original_sol[interval_choice] + original_sol[interval_choice + 1]))
 
         reward, flag = self.get_reward(new_sol, projected_original_sol, new_x, p)
+        
         training_outputs = reward
 
         return training_inputs, training_outputs, training_inputs_coarsen_l, training_inputs_coarsen_r, new_x, new_sol, interval_choice, flag
@@ -274,7 +278,7 @@ class RL_agent:
             The new set of x coordinates 
 
         p: int 
-            The number of elements in the old set of x coordinates 
+            The ratio of elements to the max number of elements
 
         Returns: 
         --------
@@ -287,14 +291,16 @@ class RL_agent:
         p_new = p + 1 / self.max_elements
         if p_new >= 1: 
             return -10, True
-        R_comp = p_new ** 0.5 / (1 - p_new) - p ** 0.5 / (1-p_new)
+        R_comp = 10 ** -3 * (1/(1-p_new)) ** 0.25#p_new ** 0.5  / (1 - p_new) - p ** 0.5 / (1-p)
         
         du = np.abs(new_sol - original_sol)
-        delta_uh = simpson(du, x=new_x)
+        dx = np.roll(new_x, -1) - new_x
+        dx[-1] = 0 
+        delta_uh = sum(du * dx)
         R_uh = delta_uh
-        
-        reward = R_uh - self.gamma_c * R_comp
-
+        #print (R_uh, R_comp)
+        reward = (R_uh - R_comp) * 10 ** 3
+        #print(reward)
         return reward, False
 
     def get_gradients(self, c, x, sol):
@@ -375,7 +381,7 @@ class RL_agent:
         with open("NN_c", "wb") as f: 
             pickle.dump(self.Q_c, f)
     
-    def evaluate(self, x, problem, u0, endtime, max_iter = 50): 
+    def evaluate(self, x, problem, u0, endtime, max_iter = 20): 
         """
         Performs adaptive mesh refinement based on the learned value function 
 
@@ -406,16 +412,20 @@ class RL_agent:
             The final solution on the refined x coordinates
         """
         new_x = copy.deepcopy(x) 
+        initial_t = problem.T
         for i in range(max_iter): 
             p = new_x.size / self.max_elements
             c = problem.c 
 
             print (f"iteration {i}")
             # solve the fluid problem on the current grid 
-            problem.T = 0
-            original_sol = problem.solve_linear_advection(u0(new_x), new_x, endtime, plotting=True)
+            problem.T = initial_t
+            original_sol = problem.solve_linear_advection(u0(new_x), new_x, endtime, plotting=False)
+            if i == max_iter - 1: 
+                return new_x, original_sol
 
-            problem.plot(original_sol, new_x)
+
+            #problem.plot(original_sol, new_x)
             gradients, delta_x = self.get_gradients(problem.c, new_x, original_sol)
             
             local_gradients = gradients
@@ -426,15 +436,14 @@ class RL_agent:
 
             size = delta_x
             average_size = np.full_like(size, np.mean(delta_x))
-
             # obtain the inputs of the neural network 
-            inputs = np.c_[local_gradients, lg, rg, avg_gradient, size, average_size, np.full_like(size, p), np.full_like(size, c)]
+            inputs = np.c_[local_gradients, avg_gradient, size, average_size, np.full_like(size, p), np.full_like(size, c)]
             r_outputs = self.Q_r.predict(inputs)
             c_outputs = self.Q_c.predict(inputs)
 
             # Decide whether to refine or not 
-            outputs_refine = r_outputs - 0.03
-            outputs_coarsen = c_outputs
+            outputs_refine = r_outputs#- np.partition(r_outputs, -4)[-4]
+            outputs_coarsen = c_outputs#- np.partition(c_outputs, -2)[-2]
             outputs_nothing = np.zeros_like(c_outputs)
 
             outputs = np.c_[outputs_refine, outputs_nothing, outputs_coarsen]
@@ -442,6 +451,7 @@ class RL_agent:
 
             temp_x = [new_x[0]]
 
+            coarsen_flag = False
             for i in range(actions.size): 
                 if actions[i] == 0: 
                     # Refine 
@@ -452,16 +462,40 @@ class RL_agent:
                     temp_x.append(new_x[i+1])
                 elif actions[i] == 2: 
                     # coarsen 
-                    if i == actions.size - 1: 
+                    if i == actions.size - 1 or coarsen_flag: 
                         temp_x.append(new_x[i+1])
-                    pass 
+                        coarsen_flag = False
+                    elif new_x[i+2] - new_x[i] > 0.2: 
+                        temp_x.append(new_x[i+1])
+                    else: 
+                        coarsen_flag = True 
+                        pass
 
             if len(temp_x) == len(new_x): 
                 return new_x, original_sol
 
+            
             new_x = np.array(temp_x)
 
         return new_x, original_sol
+    
+    def evaluate_uniform(self, no_points, problem, u0, endtime):
+        grid = np.linspace(0, 2, no_points-1)
+        sol = problem.solve_linear_advection(u0(grid), grid, endtime, plotting=False)
+        return grid, sol
+    
+    def gen_initial_condition(self, new_x):
+        return np.interp(new_x, self.interp_xs, self.interp_ys)
+    
+    def get_errors(self, exact_sol): 
+        new_x = np.linspace(0, 2, 100)
+        approx_sols = self.gen_initial_condition(new_x)
+        exact_ys = exact_sol(new_x)
+        dx = np.roll(new_x, -1) - new_x
+        dx[-1] = 0 
+        errors = (exact_ys - approx_sols) ** 2 
+        total_error = sum(errors * dx)
+        return total_error
 
     def load_NN(self, filename_r, filename_c): 
         """
@@ -509,7 +543,7 @@ def generate_data(n_points, agent):
         The generated coarsen neural network outputs 
     """
     r_inputs, r_outputs, c_inputs, c_outputs = agent.gen_training_data(np.linspace(0, 2, 5), n_points)
-    print (c_outputs)
+    print (r_outputs)
 
     with open("r_inputs", 'wb') as f: 
         pickle.dump(r_inputs, f)
@@ -525,6 +559,20 @@ def generate_data(n_points, agent):
 
     return r_inputs, r_outputs, c_inputs, c_outputs
 
+class Problem: 
+    def __init__(self, problem, initial_cond, max_points, no_steps): 
+        self.problem = problem 
+        self.initial_cond = initial_cond 
+        self.max_points = max_points 
+        self.no_steps = no_steps
+
+def square_bump(x, center=1, width=0.75):
+    left = np.tanh((x - (center - width / 2)) / (width / 10))
+    right = np.tanh((x - (center + width / 2)) / (width / 10))
+    
+    # The bump is the difference of the two transitions
+    return 0.2 * (left - right)
+
 if __name__ == "__main__": 
     """
     Either train data and run the solution, or use serialized solutions
@@ -536,33 +584,85 @@ if __name__ == "__main__":
                   Q_c = NeuralNet(n_layers = 4, hidden_neurons=64, max_iter = 5000))
     
     # Generate data for training the neural networks 
-    #r_inputs, r_outputs, c_inputs, c_outputs = generate_data(20000, rl)
+    #
 
     # Load pretrained neural nets (if already trained)
     rl.load_NN("NN_r", "NN_c")
-
+    r_inputs, r_outputs, c_inputs, c_outputs = generate_data(20000, rl)
     # Train neural nets (if not yet trained)
-    #rl.train_nn(r_inputs, r_outputs, c_inputs, c_outputs, plot_loss=True)
+
+    rl = RL_agent(0.15, 
+                  Q_r = NeuralNet(n_layers = 4, hidden_neurons=64, max_iter = 2500), 
+                  Q_c = NeuralNet(n_layers = 4, hidden_neurons=64, max_iter = 2500))
+    rl.train_nn(r_inputs, r_outputs, c_inputs, c_outputs, plot_loss=True)
 
     # Use adaptive mesh refinement to solve a given problem
-    new_x, sol = rl.evaluate(np.linspace(0, 2, 5), 
-                             LinearAdvection(1, 0.8, 0), 
-                             lambda x: 1-np.tanh(2*(1-4*(x-1/4))), 0.5)
+    # initialize the problem
+    tanh_p_25 = Problem(LinearAdvection(1, 0.05, 0), lambda x: 1-np.tanh(2*(1-4*(x - 1/4))), 25, 150)
+    tanh_p_20 = Problem(LinearAdvection(1, 0.05, 0), lambda x: 1-np.tanh(2*(1-4*(x - 1/4))), 20, 20)
+    tanh_p_15 = Problem(LinearAdvection(1, 0.05, 0), lambda x: 1-np.tanh(2*(1-4*(x - 1/4))), 15, 20)
+
+    bump_p_25 = Problem(LinearAdvection(1, 0.05, 0), lambda x: np.where(np.abs(1-x) < 0.5, 4 * np.e ** (-1/(1-4 * (x-1) ** 2)), 0), 25, 30)
+    sq_bump_p_25 = Problem(LinearAdvection(1, 0.05, 0), square_bump, 25, 30)
+    shock_p_100 = Problem(LinearAdvection(1, 0.05, 0), lambda x: np.where(x<1, 0, 1), 40, 30)
     
-    # print the number of points used 
-    print (new_x.size)
+    # Modify to choose the problem
+    p = tanh_p_25
+    
+    initial_cond = p.initial_cond 
+    no_steps = p.no_steps 
+    problem = p.problem
+    max_points = p.max_points
 
+    rl.interp_xs = np.linspace(0, 2, 100)
+    initial_ys = initial_cond(rl.interp_xs)
+    rl.interp_ys = initial_ys
+    rl.max_elements = max_points
+
+    xs = [np.linspace(0, 2, 10)] 
+    sols = [rl.gen_initial_condition(xs[-1])]
+    adaptive_errors = [0]
+    ts = [0]
+    t = 0
+
+    for i in range(no_steps):
+        new_x, sol = rl.evaluate(np.linspace(0, 2, 5), 
+                                problem, 
+                                rl.gen_initial_condition, 
+                                -1)
+        ts.append(problem.T) 
+        xs.append(new_x)
+        sols.append(sol)
+        rl.interp_xs = new_x 
+        rl.interp_ys = sol
+        TSE = rl.get_errors(lambda x: initial_cond(x-ts[-1]))
+        adaptive_errors.append(TSE)
+
+    no_points = new_x.size 
+    endtime = ts[-1]
+    problem.T = 0
+    uniform_x, uniform_sol = rl.evaluate_uniform(no_points, problem, initial_cond, endtime)
+    rl.interp_xs = uniform_x 
+    rl.interp_ys = uniform_sol
+    uniform_error = rl.get_errors(lambda x: initial_cond(x-ts[-1]))
     # plot the final solution 
-    x_ex = np.linspace(0, 2, 100)
-    u_ex = 1-np.tanh(2*(1-4*(x_ex-1/4 - 0.5)))
 
-    plt.figure(1)
-    plt.cla()
-    plt.plot(x_ex, u_ex, color = 'black')
-    plt.plot(new_x, sol, "o-")
-    plt.xlabel("x")
-    plt.ylabel("u")
+    print (f"{uniform_error = }, {adaptive_errors[-1]= }")
+    for i in range(len(xs)): 
+
+        x_ex = np.linspace(0, 2, 100)
+        u_ex = initial_cond(x_ex-problem.c * ts[i])
+
+        plt.figure(1)
+        plt.cla()
+        plt.plot(x_ex, u_ex, color = 'black')
+        plt.plot(xs[i], sols[i], "o-")
+        plt.title(f"t = {ts[i]}")
+        plt.xlabel("x")
+        plt.ylabel("u")
+        plt.pause(0.05)
+
+    plt.plot(uniform_x, uniform_sol, "o-", color="red", alpha = 0.5)
     plt.show()
-
     with open("new_x", "wb") as f: 
         pickle.dump(new_x, f)
